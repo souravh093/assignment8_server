19b38a1ab1b842109d6367bf9f17165bc6cc5ca9
Sourave Halder <souravh093@gmail.com>
2024-11-11 19:06:38 +0600

    Complete the Documentation

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from "./config";
 
 async function main() {
   const server: Server = app.listen(config.port, () => {
-    console.log(`PH HEATH SERVER LISTENING ON PORT ${config.port} 👌`);
+    console.log(`BOOKKI LISTENING ON PORT ${config.port} 👌`);
   });
 }
 
